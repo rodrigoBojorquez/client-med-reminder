edf80cfc6e1e8899dfb978d9e4246ac4716841f2
rodrigo_bojorquez <rbojorquez1620@gmail.com>
2023-10-24 19:56:32 -0500

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+function App() {
+
+  return (
+    <>
+      <p className='text-2xl font-bold text-center'>Texto prueba</p>
+    </>
+  )
+}
+
+export default App
